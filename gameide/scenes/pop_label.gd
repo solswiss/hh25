39916412754d51d0d0b830d4f8d8dd0a911cac75
solswiss <sunstingeer@gmail.com>
2025-10-18 13:22:50 -0500

    init godot with start and world

---
+++ gameide/scenes/pop_label.gd
@@ -0,0 +1,71 @@
+extends Control
+class_name PopLabel
+
+# customs
+@export var color: Color = Color.WHITE
+@export var max_pop: float = 0.5
+@export var font_size: int = 16
+@export var value: int = 0:
+	set = _update_value
+
+var digits: Dictionary[int, String] = {
+	0: "0",
+}
+
+var digit_effects: Dictionary[String, float] = {
+	"0": 0.0,
+}
+
+var tweens: Dictionary[int, Tween] = {}
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	queue_redraw()
+
+func _draw() -> void:
+	var font = get_theme_default_font()
+	var cx = 0
+	for i in digits.size():
+		var effect = digit_effects[str(i)]
+		var scale = 1.0 + effect * max_pop
+		var digit_size = font.get_string_size(digits[i],0,-1,font_size)
+		var scaled_size = digit_size * scale
+		draw_set_transform(
+			Vector2(cx,font.get_ascent(font_size)*scale-(scaled_size/2.0)),
+			0,
+			Vector2(scale,scale)
+		)
+		draw_char(
+			get_theme_default_font(),
+			Vector2.ZERO,
+			digits[i],
+			font_size,
+			lerp(color, Color.BLUE, effect)
+		)
+		cx += digit_size.x
+
+func _update_value(val):
+	if val==value: return
+	value = val
+	var str_val = "%d"%value
+	if str_val.length()>digits.size():
+		for i in (digits.size() - str_val.length()):
+			digits.erase(digits.size()-1)
+			digit_effects.erase(str(digits.size()-1))
+		for i in str_val.length():
+			if !digits.has(i):
+				digits[i] = str_val[i]
+				digit_effects[str(i)] = 1.0
+				_digit_effect_tween(i)
+			else:
+				if digits[i] != str_val[i]:
+					digits[i] = str_val[i]
+					digit_effects[str(i)] = 1.0
+					_digit_effect_tween(i)
+
+func _digit_effect_tween(digit:int):
+	if tweens.has(digit):
+		tweens[digit].kill()
+	var tween = create_tween()
+	tween.tween_property(self,"digit_effects:%d"%digit,0.0,0.5)
+	tweens[digit] = tween
