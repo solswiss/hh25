d84bf389f14ebc0978146f214245b5677146c5e5
solswiss <sunstingeer@gmail.com>
2025-10-19 09:52:29 -0500

    combo

--- gameide/scenes/States/end_scene.gd
+++ gameide/scenes/States/end_scene.gd
@@ -3,11 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("end")
 	$Control/VBoxContainer2/VBoxContainer/HBoxContainer/ScoreLabel.text = str(Global.live_score)
 	$Control/VBoxContainer2/VBoxContainer/HBoxContainer2/HighScoreLabel.text = str(Global.high_score)
+	$Control/VBoxContainer2/ReplayButton.pressed.connect(replay)
+	print("ok")
 
 
-func _on_replay_button_pressed() -> void:
+func replay():
 	print("replay")
 	get_tree().change_scene_to_file("res://scenes/Game/game.tscn")
